69762c2c0afbd39b8f42d602c337e266aa7d5a8c
Carina Jurgovsky <carina.jurgovsky@gmx.at>
2020-06-12 23:47:23 +0200

    update

--- js_CR5/script_CR5.js
+++ js_CR5/script_CR5.js
@@ -1,5 +1,7 @@
+
 var moviedata = JSON.parse(Moviedata);
-for (var i = 0; i < Moviedata.length; i++) {
-	document.getElementByClass("movie_name").innerHTML +=
-	Moviedata[0].name + "<br>";
+console.log(moviedata);
+for (var i = 0; i < moviedata.length; i++) {
+	var movie_object = moviedata[i];
+	document.getElementsByClassName("movie_name")[0].innerHTML += movie_object.name + "<br>";
 }
