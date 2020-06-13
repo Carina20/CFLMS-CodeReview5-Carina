08bec954cf7693587db241aa9eeb4e9a8839914c
Carina Jurgovsky <carina.jurgovsky@gmx.at>
2020-06-13 07:14:17 +0200

    update

--- js_CR5/script_CR5.js
+++ js_CR5/script_CR5.js
@@ -5,8 +5,8 @@ console.log (moviedataA);
 var i = 0;
 for (var i = 0; i < moviedataA.length; i++) {
 	var movie_object = moviedataA[i];
-	document.getElementById("movieA").innerHTML += movie_object.name + "<br>";
-	document.getElementById("descriptionA").innerHTML += movie_object.description + "<br>";
+	document.getElementById("movie1").innerHTML += movie_object.name + "<br>";
+	document.getElementById("description1").innerHTML += movie_object.description + "<br>";
 }
 
 
@@ -14,14 +14,14 @@ var moviedataB = JSON.parse(movieB);
 var i = 0;
 for (var i = 0; i < moviedataB.length; i++) {
 	var movie_object = moviedataB[i];
-	document.getElementById("movieB").innerHTML += movie_object.name + "<br>";
-	document.getElementById("descriptionB").innerHTML += movie_object.description + "<br>";
+	document.getElementById("movie2").innerHTML += movie_object.name + "<br>";
+	document.getElementById("description2").innerHTML += movie_object.description + "<br>";
 }
 
 var moviedataC = JSON.parse(movieC);
 var i = 0;
 for (var i = 0; i < moviedataC.length; i++) {
 	var movie_object = moviedataC[i];
-	document.getElementById("movieC").innerHTML += movie_object.name + "<br>";
-	document.getElementById("descriptionC").innerHTML += movie_object.description + "<br>";
+	document.getElementById("movie3").innerHTML += movie_object.name + "<br>";
+	document.getElementById("description3").innerHTML += movie_object.description + "<br>";
 }
